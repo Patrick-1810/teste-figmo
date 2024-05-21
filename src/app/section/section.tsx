e34efa0fd4811d05bce4d7c3639fb7d5222b9ca5
Patrick Prestes <patrickprestes977@gmail.com>
2024-05-21 16:13:01 -0300

    feat: created casino and sports

--- src/app/section/section.tsx
+++ src/app/section/section.tsx
@@ -1,13 +1,154 @@
-import "./section.css"
+import "./section.css";
+import Image from "next/image"
 
 export function Section() {
     return (
         <div className="container-section">
-            <div className="card-carroussel">
+            <div className="card-carrossel">
                 <div className="title-two">
+                    <Image
+                        src="/iconCassino.svg"
+                        alt="Icone Cassino"
+                        width={40}
+                        height={20}
+                        priority
+                    />
                     <h3>CASSINO</h3>
 
+                    <Image
+                        src="/iconControl.svg"
+                        alt="Icone Control"
+                        width={40}
+                        height={20}
+                        priority
+                    />
+                </div>
+                <div className="imagem-carrossel">
+                    <Image
+                        src="/originals.png"
+                        alt="Icone Original"
+                        width={149}
+                        height={200}
+                        priority
+                    />
+                    <Image
+                        src="/slots.png"
+                        alt="Icone Slots"
+                        width={149}
+                        height={200}
+                        priority
+                    />
+                    <Image
+                        src="/game.png"
+                        alt="Icone Game"
+                        width={149}
+                        height={200}
+                        priority
+                    />
+                    <Image
+                        src="/live.png"
+                        alt="Icone Live"
+                        width={149}
+                        height={200}
+                        priority
+                    />
+                    <Image
+                        src="/table.png"
+                        alt="Icone Table"
+                        width={149}
+                        height={200}
+                        priority
+                    />
+                    <Image
+                        src="/blackjack.png"
+                        alt="Icone Blackjack"
+                        width={149}
+                        height={200}
+                        priority
+                    />
+                    <Image
+                        src="/roulette.png"
+                        alt="Icone Roulette"
+                        width={149}
+                        height={200}
+                        priority
+                    />
+                </div>
+            </div>
 
+            <div className="card-esportes">
+                <div className="title-three">
+                    <Image
+                        src="/iconEsportes.svg"
+                        alt="Icone Esportes"
+                        width={40}
+                        height={20}
+                        priority
+                    />
+                    <h3>ESPORTES</h3>
+
+                </div>
+                <div className="elements-esportes">
+                    <div className="element-1">
+                        <Image
+                            src="/iconeTenis.svg"
+                            alt="Icone Tenis"
+                            width={39.56}
+                            height={40}
+                            priority
+                        />
+                        <h3>TÊNIS DE MESA</h3>
+                    </div>
+                    <div className="element-2">
+                        <Image
+                            src="/iconeVolei.svg"
+                            alt="Icone Volei"
+                            width={39.56}
+                            height={40}
+                            priority
+                        />
+                        <h3>VÔLEIBOL</h3>
+                    </div>
+                    <div className="element-3">
+                        <Image
+                            src="/iconeBoxe.svg"
+                            alt="Icone Boxe"
+                            width={39.56}
+                            height={40}
+                            priority
+                        />
+                        <h3>BOXE</h3>
+                    </div>
+                    <div className="element-4">
+                        <Image
+                            src="/iconeGolfe.svg"
+                            alt="Icone Golfe"
+                            width={39.56}
+                            height={40}
+                            priority
+                        />
+                        <h3>GOLFE</h3>
+                    </div>
+                    <div className="element-5">
+                        <Image
+                            src="/iconeMma.svg"
+                            alt="Icone MMA"
+                            width={39.56}
+                            height={40}
+                            priority
+                        />
+                        <h3>MMA</h3>
+                    </div>
+                    <div className="element-6">
+                        <Image
+                            src="/iconeSinuca.svg"
+                            alt="Icone Sinuca"
+                            width={39.56}
+                            height={40}
+                            priority
+                        />
+                        <h3>SINUCA</h3>
+                    </div>
                 </div>
             </div>
         </div>
